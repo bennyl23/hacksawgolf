7acb5caf9a4040b4e6bf0d2bc1a614f1a969ea40
Ben Lefebvre <bennyl23@yahoo.com>
2017-02-14 12:37:16 -0500

    commit

--- hacksawgolf/settings.py
+++ hacksawgolf/settings.py
@@ -106,7 +106,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/projects/hacksawgolf/hacksawgolf/hacksawgolf/static/'
+STATIC_ROOT = '/hacksawgolf/static/'
 
 # Static files paths
 STATICFILES_DIRS = (
