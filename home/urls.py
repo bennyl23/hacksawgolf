10e7db87aa424aa8c91823e33cd8fe63b686d917
Ben Lefebvre <bennyl23@yahoo.com>
2017-02-14 22:53:27 -0500

    commit

--- home/urls.py
+++ home/urls.py
@@ -5,5 +5,7 @@ from login.functions import signed_in_only
 
 urlpatterns = patterns('',
     # / or /home/
-    url(r'^$', signed_in_only(views.HomeView.as_view()), name='home')
+    url(r'^$', signed_in_only(views.HomeView.as_view()), name='home'),
+    # /home/3/
+    url(r'^(?P<tournament_week>\d+)/$', signed_in_only(views.HomeView.as_view()), name='home'),
 )
--- home/views.py
+++ home/views.py
@@ -1,9 +1,48 @@
 from django.views.generic import TemplateView
 from tournament.models import Tournament
+from register.models import User
+from team.models import TeamMember
+from django.db.models import Sum
 
 
 class HomeView(TemplateView):
     template_name = 'home/home.html'
 
-    def current_tournament(self):
-        return Tournament.objects.current_tournament()
+    def user(self):
+        return User.objects.get(user_id=self.request.session['user_id'])
+
+    def tournament(self):
+        # if user is navigating to a specific tournament (e.g. /home/4/)
+        if 'tournament_week' in self.kwargs:
+            try:
+                tournament = Tournament.objects.get(tournament_week=int(self.kwargs['tournament_week']))
+            except Tournament.DoesNotExist:
+                tournament = Tournament.objects.current_tournament()
+            finally:
+                return tournament
+        # default to current tournament
+        else:
+            return Tournament.objects.current_tournament()
+
+    def tournament_count(self):
+        return Tournament.objects.count()
+
+    def team(self):
+        response = {}
+
+        tournament = self.tournament()
+
+        team_members = TeamMember.objects.filter(
+                                                     user_id=self.request.session['user_id'],
+                                                     tournament_id=tournament.tournament_id,
+                                                     ).order_by(
+                                                                '-salary',
+                                                                'golfer_lname'
+                                                                )
+
+        salary_total = team_members.aggregate(Sum('salary'))
+
+        response['team_members'] = team_members
+        response['salary_total'] = salary_total
+
+        return response
