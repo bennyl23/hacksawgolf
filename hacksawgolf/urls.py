1b63dac516225c530827f5a72d3cb15d83a4733b
Ben Lefebvre <bennyl23@yahoo.com>
2017-02-15 21:24:29 -0500

    commit

--- hacksawgolf/urls.py
+++ hacksawgolf/urls.py
@@ -18,8 +18,10 @@ urlpatterns = patterns('',
     (r'^tinymce/', include('tinymce.urls')),
 )
 
+'''
 if settings.DEBUG:
     import debug_toolbar
     urlpatterns += patterns('',
         url(r'^__debug__/', include(debug_toolbar.urls)),
     )
+'''
