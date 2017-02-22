4d36a18c177ebb87435bcf3e1561e4f47c5d342a
Ben Lefebvre <bennyl23@yahoo.com>
2017-02-21 23:49:45 -0500

    commit

--- register/admin.py
+++ register/admin.py
@@ -4,7 +4,7 @@ from register.models import User
 
 class UserAdmin(admin.ModelAdmin):
     fields = ['user_email', 'user_team_name', 'user_real_name', 'user_af1', 'user_af2', 'user_paid']
-    list_display = ['user_email', 'user_team_name', 'user_real_name', 'user_paid']
+    list_display = ['user_email', 'user_team_name', 'user_real_name', 'user_af1', 'user_af2', 'user_paid']
     list_editable = ['user_paid']
     list_filter = ['user_paid']
     list_per_page = 1000
