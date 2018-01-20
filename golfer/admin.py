d5f146029a675bef453cb745de4556f0a75ad662
Ben Lefebvre <bennyl23@yahoo.com>
2018-01-20 11:39:00 -0500

    commit

--- golfer/admin.py
+++ golfer/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from golfer.models import Golfer
 from tournament.models import TournamentParticipant
+from team.models import Team
+from decimal import Decimal
 
 
 # custom golfer filter to see who has or has not been a tournament participant
@@ -32,11 +34,21 @@ class GolferHasPlayedFilter(admin.SimpleListFilter):
 
 class GolferAdmin(admin.ModelAdmin):
     fields = ['golfer_fname', 'golfer_lname', 'golfer_mi', 'default_salary', 'original_salary']
-    list_display = ['golfer_lname', 'golfer_fname', 'default_salary', 'original_salary']
+    list_display = ['golfer_lname', 'golfer_fname', 'default_salary', 'original_salary', 'golfer_picked_per_tournament_played']
     list_editable = ['default_salary', 'original_salary']
     list_per_page = 10000
     ordering = ('golfer_lname', 'golfer_fname')
     list_filter = [GolferHasPlayedFilter]
 
+    def golfer_picked_per_tournament_played(self, obj):
+        num_tournaments_played = TournamentParticipant.objects.filter(golfer__golfer_id=obj.golfer_id).count()
+        if num_tournaments_played == 0:
+            return 0
+        num_times_picked = Team.objects.filter(golfer__golfer_id=obj.golfer_id).count()
+        num_times_picked_per_tournament = round(num_times_picked/float(num_tournaments_played), 1)
+        if num_times_picked_per_tournament.is_integer():
+            return int(num_times_picked_per_tournament)
+        return num_times_picked_per_tournament
+    golfer_picked_per_tournament_played.short_description = 'Picked/Tourn. played'
 
 admin.site.register(Golfer, GolferAdmin)
